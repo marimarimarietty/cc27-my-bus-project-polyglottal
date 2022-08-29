7dd9b9f8024d40f11d1732d6d9bb7663f22949b5
marimarimarietty <cnblue.yonghwa.maroon5@gmail.com>
2022-08-29 14:17:00 +0900

    feat: create schemas

--- api/routers/notifications.py
+++ api/routers/notifications.py
@@ -1,23 +1,24 @@
+from typing import List
 from fastapi import APIRouter
+import api.schemas.notifications as notifications_schema
 
 router = APIRouter()
 
-
-@router.get("/notifications")
+@router.get("/notifications", response_model=List[notifications_schema.Notifications])
 async def list_notification():
-    pass
+  return [notifications_schema.Notifications(id=1, busroute="odpt.Busroute:Toei.NM01", direction="3", busstop="odpt.BusstopPole:Toei.NipponKagakuMiraikan.2546.2", busid="odpt.Bus:Toei.NM01.4601.2.T280", userid="1", busanduserid="odpt.Bus:Toei.NM01.4601.2.T2801")]
 
 
-@router.post("/notifications")
-async def create_notifications():
-    pass
+@router.post("/notifications", response_model=notifications_schema.NotifiCreateResponse)
+async def create_notifications(notifi_body: notifications_schema.NotifiCreate):
+    return notifications_schema.NotifiCreateResponse(id=1, **notifi_body.dict())
 
 
-@router.patch("/notifications/{busanduser_id}")
-async def update_notifications():
-    pass
+@router.patch("/notifications/{busanduser_id}", response_model=notifications_schema.NotifiCreateResponse)
+async def update_notifications(notifi_id: int, notifi_body: notifications_schema.NotifiCreate):
+    return notifications_schema.NotifiCreateResponse(id=notifi_id, **notifi_body.dict())
 
 
-@router.delete("/notifications/{busanduser_id}")
-async def delete_busanduser():
-    pass
+@router.delete("/notifications/{busanduser_id}", response_model=None)
+async def delete_notifications(notifi_id: int):
+    return
---
+++ api/schemas/notifications.py
@@ -0,0 +1,29 @@
+from doctest import Example
+from typing import Optional
+
+from pydantic import BaseModel, Field
+
+class NotifiBase(BaseModel):
+    busroute: Optional[str] = Field(None, example="odpt.Busroute:Toei.NM01")
+    direction: Optional[str] = Field(None, example="3")
+    busstop: Optional[str] = Field(None, example="odpt.BusstopPole:Toei.NipponKagakuMiraikan.2546.2")
+    busid: Optional[str] = Field(None, example="odpt.Bus:Toei.NM01.4601.2.T280")
+    userid: Optional[str] = Field(None, example="1")
+    busanduserid: Optional[str] = Field(None, example="odpt.Bus:Toei.NM01.4601.2.T2801")
+
+class NotifiCreate(NotifiBase):
+  pass
+
+class NotifiCreateResponse(NotifiCreate):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+class Notifications(NotifiBase):
+    id: int
+    done: bool = Field(False, description="flag")
+
+    class Config:
+      orm_mode = True
+
