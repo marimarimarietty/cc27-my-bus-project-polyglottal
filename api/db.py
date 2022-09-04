2e4930b24b863088b0d2d7f6d5ceb9e333c80d40
marimarimarietty <cnblue.yonghwa.maroon5@gmail.com>
2022-09-04 18:27:16 +0900

    feat: update db file

--- api/db.py
+++ api/db.py
@@ -1,7 +1,11 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-ASYNC_DB_URL = "mysql+aiomysql://root@db:3306/bus?charset=utf8"
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+ASYNC_DB_URL = os.environ['ASYNC_DB_URL']
 
 
 async_engine = create_async_engine(ASYNC_DB_URL, echo=True)
